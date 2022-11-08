c65572db00692e1f5fe8dcd5aedcf62c3ac749a8
traplinboss0 <nmoreno429@misena.edu.co>
2022-11-08 09:38:31 -0500

    Este es un pequeño avance en el backend

---
+++ Registrar.php
@@ -0,0 +1,8 @@
+<?php
+$nombres=$_POST['Nombres']
+$apellidos=$_POST['Apellidos']
+$email=$_POST['Email']
+$password=$_POST['Contraseña']
+$password2=$_POST['Contraseña2']
+$tel=$_POST['Tel']
+$telf=$_POST['TelF']
