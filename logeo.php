26446e795ade1adde7ef9ca1091faa7dfc66567c
traplinboss0 <nmoreno429@misena.edu.co>
2022-11-15 02:00:51 -0500

    se intento pero se va alograr mañna

--- logeo.php
+++ logeo.php
@@ -1,11 +1,10 @@
 <?php
 include_once('db.php');
 $usuario=$_POST['Usuario'];
-$contraseña=$_POST['password'];
 
 
 $conectar=conn();
-$consulta="SELECT*FROM usuarios where email = '$usuario' and contrasena='$contraseña'";
+$consulta="SELECT*FROM usuarios where email = '$usuario'";
 $resultado=mysqli_query($conectar,$consulta);
 
 
@@ -13,11 +12,12 @@ $resultado=mysqli_query($conectar,$consulta);
 $filas=mysqli_num_rows($resultado);
 
 if ($filas) {
-    header("location:html/dashboard.html");
-}else{
-    header("location:html/Iniciom.html");
-
+    while($fila = mysqli_fetch_array( $resultado )){
+        echo $fila['nombres'];
+        echo $fila['apellidos'];
+    }
 }
+
 
 mysqli_free_result($resultado);
 mysqli_close($conectar);
